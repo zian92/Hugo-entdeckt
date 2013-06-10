01f854cedb7177ed2cf62487039230199e180f5e
zian92 <xyxyxyz@hotmail.de>
2013-06-10 09:13:28 +0200

    hi du ei

--- App/src/de/hugo/hugo_entdeckt/LaunchActivity.java
+++ App/src/de/hugo/hugo_entdeckt/LaunchActivity.java
@@ -2,14 +2,41 @@ package de.hugo.hugo_entdeckt;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class LaunchActivity extends Activity {
+	CharSequence text = "Hello toast!";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_launch);
+
+		final Button button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				Context context = getApplicationContext();
+				int duration = Toast.LENGTH_LONG;
+
+				Toast toast = Toast.makeText(context, text, duration);
+				toast.show();
+			}
+		});
+
+		final CheckBox cb = (CheckBox) findViewById(R.id.checkBox1);
+		cb.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				if (cb.isChecked())
+					text = " hi du ei";
+				else text = "wrwerwer";
+			}
+		});
 	}
 
 	@Override
